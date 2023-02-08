291c75703c4111ce2b30d6da51c683c4736192c2
clintonbempah <20097793@mail.wit.ie>
2023-02-08 17:46:49 +0000

    Refactoring menu code to use other constructs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import java.util.*
 import java.lang.System.exit
+import java.util.*
 
 val scanner = Scanner(System.`in`)
 
@@ -8,51 +8,54 @@ fun main(args: Array<String>) {
 }
 
 fun mainMenu() : Int {
-    println("")
-    println("--------------------")
-    println("NOTE KEEPER APP")
-    println("--------------------")
-    println("NOTE MENU")
-    println("  1) Add a note")
-    println("  2) List all notes")
-    println("  3) Update a note")
-    println("  4) Delete a note")
-    println("--------------------")
-    println("  0) Exit")
-    println("--------------------")
-    print("==>> ")
+    print(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
     return scanner.nextInt()
 }
+
+
 fun runMenu() {
     do {
         val option = mainMenu()
         when (option) {
-            1  -> addNote()
-            2  -> listNotes()
-            3  -> updateNote()
-            4  -> deleteNote()
-            0  -> exitApp()
-            else -> println("Invalid option entered: " + option)
+            1 -> addNote()
+            2 -> listNotes()
+            3 -> updateNote()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> println("Invalid option entered: $option")
         }
     } while (true)
 }
-fun addNote(){
+
+fun addNote() {
     println("You chose Add Note")
 }
 
-fun listNotes(){
+fun listNotes() {
     println("You chose List Notes")
 }
 
-fun updateNote(){
+fun updateNote() {
     println("You chose Update Note")
 }
 
-fun deleteNote(){
+fun deleteNote() {
     println("You chose Delete Note")
 }
 
-fun exitApp(){
+fun exitApp() {
     println("Exiting...bye")
     exit(0)
 }
